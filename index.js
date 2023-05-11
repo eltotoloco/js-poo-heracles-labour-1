a3bd28e17e093fde997d31345e4b43a620430ae1
moreaudamien <moreau_student2023@wilder.school>
2023-05-11 11:30:50 +0200

    partie 1

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+//  créer deux instances de la classe Figther : heracles et Nemean lion 
+//  heracles a une force de 20 et une dextérité de 6
+//  Nemean lion a une force de 11 et une dextérité de 13
+//  heracles et Nemean lion ont chacun 100 points de vie
+
+
+
+const Fighter = require("./src/Fighter");
+
+const heracles = new Fighter("🧔Heracles", 20, 6, 100);
+
+const nemeanLion = new Fighter("🦁Nemean Lion", 11, 13, 100);
+
+let round = 1;
+
+while (heracles.health > 0 && nemeanLion.health > 0) {
+    console.log(`Round ${round}:`);
+    heracles.fight(lion);
+    lion.fight(heracles);
+    console.log("");
+    round++;
+  }
+  
+  if (heracles.health > 0) {
+    console.log(`${heracles.name} a vaincu ${nemeanLion.name} ! 🏆`);
+    console.log(`${nemeanLion.name} est mort 💀`);
+  } else {
+    console.log(`${nemeanLion.name} a vaincu ${heracles.name} ! 🏆`);
+    console.log(`${heracles.name} est mort 💀`);
+  }
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,34 @@
 /* Fighter class definition */
+
+// une constante MAX_LIFE = 100: les combattants possèdent 100 points de vie max.
+
+const MAX_LIFE = 100;
+
+// une classe Fighter avec les propriétés : name ; strength ; dexterity ; life
+
+
+class Fighter {
+    constructor(name, strength, dexterity){
+this.name= name;
+this.strength = strength;
+this.dexterity = dexterity;
+this.life = MAX_LIFE;
+}
+fight(defender) {
+    const attackerDamage = Math.floor(Math.random() * this.strength) + 1;
+    const mitigatedDamage = Math.max(attackerDamage - defender.dexterity, 0);
+    defender.life = Math.max(defender.life - mitigatedDamage, 0);
+    //les points de vie du défenseur ne peuvent tomber en dessous de zéro, rappelez vous bien de ça.
+    console.log(
+      `${this.name} attacks ${defender.name} for ${mitigatedDamage} damage. ${defender.name} has ${defender.health} health remaining.`
+    );
+    }
+}
+
+module.exports = Fighter;
+
+// faire une méthode fight() qui vous permet d'attaquer l'ennemi. Pour déterminer qui vous attaquez, la méthode fight() prend en paramètre un objet de type Fighter (on peut l'appeller defender) ! Dans le corps de la méthode, vous calculerez en premier les points de dommage de l'attaquant :
+
+
+
+  
