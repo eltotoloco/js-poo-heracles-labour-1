a1b1c4798186fd505e15f2d08af03e734368d448
Missant <damien.missant@reseau.sncf.fr>
2023-05-11 12:11:04 +0200

    kill the cat

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const Fighter =  require('./src/Fighter.js');
+
+
+const heracles = new Fighter ("🧔heracles", 20, 6);
+const nemean = new Fighter ("🦁nemean", 11, 13);
+
+const roundDisplay = (fighter1, fighter2) => {
+    return `${fighter1.name} 🗡️  ${fighter1.life} 💙 ${fighter2.name} : ${fighter2.life}`;
+  };
+
+let round = 0;
+while (heracles.isAlive() && nemean.isAlive()) {
+  console.log(`🕛 Round #${round}`);
+
+  heracles.fight(nemean);
+  console.log(roundDisplay(heracles, nemean));
+
+  //Boar can fight only if he is still alive after heracles strike
+  if (nemean.isAlive()) {
+    nemean.fight(heracles);
+    console.log(roundDisplay(nemean, heracles));
+  }
+
+  round++;
+}
+
+const score = (fighter1, fighter2) => {
+    return fighter1.isAlive() ? {
+      winner: fighter1,
+      looser: fighter2,
+    } : {
+      winner: fighter2,
+      looser: fighter1
+    };
+  };
+
+const result = score(heracles, nemean);
+
+console.log(`💀 ${result.looser.name} is dead`);
+console.log(`🏆 ${result.winner.name} wins (💙 ${result.winner.life} )`);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,35 @@
 /* Fighter class definition */
+const maxLife = 100;
+class Fighter {
+    constructor(name, strength, dexterity){
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = maxLife;
+    }
+
+    fight(defender){
+        let attackPoints = this.getRandomHit(this.getDamage());
+        let damages = Math.max(attackPoints - defender.getDefense(), 0);
+        defender.life = Math.max(defender.life - damages, 0);
+    }
+
+    getDamage(){
+        return this.strength;
+    }
+
+    getDefense(){
+        return this.dexterity;
+    }
+
+
+    getRandomHit(max){
+        return 1 + Math.floor(Math.random() * max);
+    }
+
+    isAlive() {
+        return this.life > 0
+      }
+
+}
+module.exports = Fighter;
