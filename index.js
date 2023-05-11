51893e68c09e1219bb587f8fff4905cea2faf9e0
Marcos Martins <marcos.martins@sncf.fr>
2023-05-11 11:57:22 +0200

    first attack

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const {Fighter, MAX_LIFE} = require('./src/Fighter.js');
+
+const heracles = new Fighter ('🥷', 'Heracles', 20, 6);
+const nemeanLion = new Fighter ('🦁', 'Nemean Lion', 11, 13);
+
+/*console.log(heracles);
+console.log(nemeanLion);*/
+
+console.log(`${heracles.name}: ${heracles.life} 💙`);
+console.log(`${nemeanLion.name}: ${nemeanLion.life} 💙`);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,22 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+class Fighter {
+    constructor(name, strength, dexterity){
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = MAX_LIFE;
+    }
+
+    fight(defender){
+        const damage = this.strength - defender.dexterity;
+        console.log(`${this.name} attaque ${defender.name} et inflige ${damage} points de dommage`);
+    }
+
+}
+heracles.fight(nemeanLion);
+
+
+module.exports = {Fighter, MAX_LIFE};  
+
