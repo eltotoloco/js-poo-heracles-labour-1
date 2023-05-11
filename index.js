9da16f121ddc79793752a100ad7b3de15610785b
mathieu coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-05-11 11:41:57 +0200

    First commit with code working but too much point for Nemean Lion. Work in progress

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+
+const Fighter = require('./src/Fighter.js');
+
+const heracles = new Fighter("🧔 Heracles", 20, 6);
+const nemeanLion = new Fighter("🦁 Nemean Lion", 11, 13);
+
+console.log(`${heracles.name} - Life: ${heracles.life} 💙`);  
+console.log(`${nemeanLion.name} - Life: ${nemeanLion.life} 💙`);
+
+
+let round = 1;
+while (heracles.life > 0 && nemeanLion.life > 0) {
+    console.log(`🕛 Round ${round}`);
+    heracles.fight(nemeanLion);
+    if (nemeanLion.life > 0) {
+        nemeanLion.fight(heracles);
+    }
+    round++;
+    }
+    if (heracles.life > 0) {
+        console.log(`🏆 ${heracles.name} wins!`);
+        console.log(`💀 ${nemeanLion.name} is dead!`);
+    }
+    else {
+        console.log(`🏆 ${nemeanLion.name} wins!`);
+        console.log(`💀 ${heracles.name} is dead!`);
+    }
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,22 @@
-/* Fighter class definition */
+const maxLife = 100;
+class Fighter {
+  constructor(name, strength, dexterity, life) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = maxLife;
+  }
+  fight(defender){
+    let damage = Math.floor(Math.random() * this.strength) + 1;
+    let newLife = defender.life - (damage - defender.dexterity);
+    if (newLife < 0) {
+      newLife = 0;
+    }
+    defender.life = newLife;
+    console.log(`${this.name} attack ${defender.name} !!🗡️`);
+    console.log(`${defender.name} has lost ${damage - defender.dexterity} life points!! ❣️`);
+    console.log(`${defender.name} has now ${defender.life} life points remaining!! ❤️‍🩹`);
+  }
+}
+
+module.exports = Fighter;
