4b021eccf990de6285366c8fe0a955a42da72d14
Matt-XLII <durouchet.mathieu@gmail.com>
2023-05-11 10:55:59 +0200

    adding the answer to the workshop + bonus

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+
+const Fighter = require('./src/Fighter.js');
+const Max_Life = 100
+let Heracles = new Fighter("👨 Heracles", Max_Life, 20, 6)
+let Lion = new Fighter("🦁 Nemean Lion", Max_Life, 11, 13)
+
+
+function fightToDeath(fighter1, fighter2){
+    let roundNumber = 1
+    while(fighter1.isAlive() && fighter2.isAlive()){
+        fighter1.fight(fighter2)
+        fighter2.fight(fighter1)
+        console.log(`⌛ Round #${roundNumber}`)
+        console.log(`${fighter1.Name} 🗡️ ${fighter2.Name} 💙 ${fighter2.Name}: ${fighter2.Life}`)
+        if (fighter2.isAlive()){
+            console.log(`${fighter2.Name} 🗡️ ${fighter1.Name} 💙 ${fighter1.Name}: ${fighter1.Life}`)
+        }
+        roundNumber ++
+    }
+    if (fighter1.isAlive() ){
+        console.log(`${fighter1.Name} won the fight, ${fighter2.Name} was defeated`)
+    }
+    if (fighter2.isAlive()){
+        console.log(`${fighter2.Name} won the fight, ${fighter1.Name} was defeated`)
+    }
+}
+
+fightToDeath(Heracles, Lion)
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,28 @@
-/* Fighter class definition */
+
+class Fighter {
+    constructor(Name, Life, Strength, Dex){
+    this.Name = Name
+    this.Life = Life
+    this.Strength = Strength
+    this.Dex = Dex
+    }
+fight(defender){
+    if (this.isAlive()){
+    let dmgDealt = Math.floor(Math.random() * this.Strength) -  defender.Dex
+    if (dmgDealt < 0){
+        dmgDealt = 0
+    }
+    defender.Life -= dmgDealt
+    if (defender.Life < 0){
+        defender.Life = 0
+    }
+}
+}
+isAlive(){
+    while(this.Life > 0){
+    return true}
+}
+}
+
+
+module.exports = Fighter;
