14409eb6cc157e984dae3539b0e906964428483a
Bensadia Omar <bensadia.omar@gmail.com>
2023-05-11 10:30:29 +0200

    Ajout de la class et des fighter

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require('./src/Fighter');
+
+
+const Heracles = new Fighter('Heracles', 20, 6);
+const Nemean = new Fighter('Nemean', 11, 13);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,16 @@
 /* Fighter class definition */
+const maxLife = 100;
+
+class Fighter{
+    constructor(name,strength,dexterity,life){
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = maxLife;
+    }
+}
+
+
+
+
+module.exports = Fighter;
