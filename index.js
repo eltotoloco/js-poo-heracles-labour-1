cd0922cc2d467f456983ebb21ee9f73a6163a1e2
Marcos Martins <marcos.martins@sncf.fr>
2023-05-11 15:12:35 +0200

    WS the End, youpiiiiii

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
 
-const {Fighter, MAX_LIFE} = require('./src/Fighter.js');
+const { Fighter } = require('./src/Fighter');
 
-const heracles = new Fighter ('🥷', 'Heracles', 20, 6);
-const nemeanLion = new Fighter ('🦁', 'Nemean Lion', 11, 13);
+const heracles = new Fighter('🧔 Heracles', 20, 6);
+const nemeanLion = new Fighter('🦁 Nemean Lion', 11, 13);
 
-/*console.log(heracles);
-console.log(nemeanLion);*/
+console.log('Début du combat à mort !')
 
-console.log(`${heracles.name}: ${heracles.life} 💙`);
-console.log(`${nemeanLion.name}: ${nemeanLion.life} 💙`);
+let round = 1;
+while (heracles.isAlive() > 0 && nemeanLion.isAlive() > 0){
+    console.log(`Round ${round} heure`);
+    heracles.fight(nemeanLion);
+
+    if (nemeanLion.life > 0){
+        nemeanLion.fight(heracles);
+    }
+
+    round++;
+}
+
+console.log(`Fin du combat !`);
+
+if (heracles.life > 0) {
+    console.log(`🏆 ${heracles.name} est le vainqueur !`);
+    console.log(`💀 ${nemeanLion.name} a perdu !.`);
+  } else {
+    console.log(`🏆 ${nemeanLion.name} est le vainqueur !`);
+    console.log(`💀 ${heracles.name} a perdu ! connard !.`);
+  }
+
--- src/Fighter.js
+++ src/Fighter.js
@@ -7,15 +7,25 @@ class Fighter {
         this.strength = strength;
         this.dexterity = dexterity;
         this.life = MAX_LIFE;
+        
     }
 
     fight(defender){
-        const damage = this.strength - defender.dexterity;
-        console.log(`${this.name} attaque ${defender.name} et inflige ${damage} points de dommage`);
+        const damage = Math.floor(Math.random() *this.strength) +1; 
+        const mitigatedDamage = Math.max(0, damage - defender.dexterity);
+
+        defender.life -= mitigatedDamage;
+        defender.life = Math.max(0, defender.life);
+
+        console.log(`${this.name} attaque ${defender.name} et inflige ${mitigatedDamage} points de dommage`);
+        console.log(`${defender.name} a maintenant ${defender.life} points de vie.`);
+    }
+    isAlive(){
+        return this.life > 0;
     }
 
 }
-heracles.fight(nemeanLion);
+
 
 
 module.exports = {Fighter, MAX_LIFE};  
