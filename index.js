b5352b473f7e29d380e6a2126590f0df68f53697
Clourc <corbino.lucas@gmail.com>
2023-05-11 10:38:07 +0200

    Finished Workshop, pimped the code a bit

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 // First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+// use your Fighter class here
+const Fighter = require('./src/Fighter');
+
+const heracles = new Fighter('Heracles', 20, 6, 5);
+const lion = new Fighter('Nemean Lion', 11, 13, 9);
+let round = 1;
+
+while(heracles.isAlive() && lion.isAlive()){
+  console.log(`Round n°${round}`);
+  heracles.fight(lion);
+  lion.fight(heracles);
+  round++;
+}
+
+if(lion.isAlive()){
+  console.log(`${heracles.name} is dead`);
+  console.log(`${lion.name} wins ! (${lion.life}HP left)`);
+} else if(heracles.isAlive()) {
+  console.log(`${lion.name} is dead`);
+  console.log(`${heracles.name} wins ! (${heracles.life}HP left)`);
+} else {
+  console.log(`Double kill ! Both ${heracles.name} and ${lion.name} died !`);
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,54 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter {
+    name;
+    strength;
+    dexterity;
+    luck;
+
+    constructor(name, strength, dexterity, luck){
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.luck = luck;
+        this.life = MAX_LIFE;
+    }
+
+    fight(defender){
+        let damageDealt = Math.floor(Math.random() * (this.strength) + 1);
+        let criticalHitTest = Math.floor(Math.random() * (101 - 1) + 1);
+        let criticalHit = false;
+
+        if(criticalHitTest <= this.luck){
+            damageDealt *= 2 + 1;
+            criticalHit = true;
+        }
+
+
+        if(damageDealt - defender.dexterity < 0){
+            damageDealt = 0;
+        } else {
+            damageDealt = (damageDealt - defender.dexterity);
+            defender.life -= damageDealt;
+            if(defender.life < 0){
+                defender.life = 0;
+            }
+        }
+
+        if(criticalHit){
+            console.log(`${this.name} attacks ${defender.name} and inflicts a critical hit dealing ${damageDealt} points of damage ! Double damage ! ${defender.name} remaining HP : ${defender.life}`);
+        } else {
+            console.log(`${this.name} attacks ${defender.name} and deal ${damageDealt} damage point. ${defender.name} remaining HP : ${defender.life}`);
+        }
+    }
+
+    isAlive(){
+        if(this.life > 0){
+            return true;
+        }
+        return false;
+    }
+}
+
+module.exports = Fighter;
