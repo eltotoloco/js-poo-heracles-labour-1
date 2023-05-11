0b35085463cb15aaa9a825c92790311c85e80499
Emili084 <128594426+Emili084@users.noreply.github.com>
2023-05-11 12:26:20 +0200

    update

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const Fighter = require("./src/Fighter.js");
+
+const heracles = new Fighter("🧔 Heracles", 20, 6);
+const nemeanLion = new Fighter("🦁 Nemean Lion", 11, 13);
+let round = 1;
+
+while (heracles.isAlive() && nemeanLion.isAlive()) {
+  console.log(`Round ${round}`);
+  
+  // Heracles attacks the Nemean Lion
+  console.log(`${heracles.name} attacks ${nemeanLion.name}`);
+  heracles.fight(nemeanLion);
+  console.log(`${heracles.name} remaining life: ${heracles.life}`);
+  console.log(`${nemeanLion.name} remaining life: ${nemeanLion.life}`);
+  
+  if (!nemeanLion.isAlive()) {
+    break; // Heracles won, stop the fight
+  }
+  
+  // Nemean Lion attacks Heracles
+  console.log(`${nemeanLion.name} attacks ${heracles.name}`);
+  nemeanLion.fight(heracles);
+  console.log(`${heracles.name} remaining life: ${heracles.life}`);
+  console.log(`${nemeanLion.name} remaining life: ${nemeanLion.life}`);
+
+  if (!heracles.isAlive()) {
+    break; // Heracles won, stop the fight
+  }
+  
+  round++;
+}
+
+if (heracles.isAlive()) {
+  console.log(`\n🏆 ${heracles.name} is the winner! 💀 ${nemeanLion.name} is defeated.`);
+} else {
+  console.log(`\n🏆 ${nemeanLion.name} is the winner! 💀 ${heracles.name} is defeated.`);
+}
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,29 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+class Fighter {
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+  fight(defender) {
+    let damage = Math.floor(Math.random() * this.strength) - defender.dexterity;
+    if (damage <= 0) {
+      damage = 0;
+      //console.log(`${this.name} missed ${defender.name}`);
+    }
+
+    if (defender.life - damage <= 0) {
+        defender.life = 0;
+      //console.log(`${defender.name} is dead`);
+    } else {
+      defender.life -= damage;
+      //console.log(`${this.name} deals ${damage} damage to ${defender.name}`);
+    }
+  }
+  isAlive() {
+    return this.life > 0;
+  }
+}
+module.exports = Fighter;
