eac57185409ebd2a76c91dabce59e55e7402bbdf
bryce-sncf <bryce.marcol@sncf.fr>
2023-05-11 10:08:15 +0200

    prepare OK

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require('./src/Fighter');
+
+
+const Heracles = new Fighter('Heracles', 20, 6);
+const Nemean = new Fighter('Nemean', 11, 13);
+
+
+console.log(`1st Hero : ${Heracles.name}, ${Heracles.life} PV.`)
+console.log(`2nd Hero : ${Nemean.name}, ${Nemean.life} PV.`)
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,14 @@
 /* Fighter class definition */
+const maxLife = 100;
+
+class Fighter{
+    constructor(name, strength, dexterity){
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = maxLife;
+
+    }
+}
+
+module.exports = Fighter;
