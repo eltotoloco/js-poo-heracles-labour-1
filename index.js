1a2e6b1cad762b09ac236335cd035bd59e667652
Bensadia Omar <bensadia.omar@gmail.com>
2023-05-11 10:41:18 +0200

    Nom des personnages et point de vie

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 const Fighter = require('./src/Fighter');
 
 
-const Heracles = new Fighter('Heracles', 20, 6);
-const Nemean = new Fighter('Nemean', 11, 13);
+const heracles = new Fighter('Heracles', 20, 6);
+const nemean = new Fighter('Nemean', 11, 13);
+
+console.log(`Je suis ${heracles.name} mon nom parle pour mois avec ${heracles.life} point de vie`)
+console.log(`Je suis ${nemean.name} Féroce Lion avec ${nemean.life} point de vie`)
